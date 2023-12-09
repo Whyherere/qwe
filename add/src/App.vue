c327c222346d9ed041dbdae8763224070c69fb83
Whyhere <148620619+Whyherere@users.noreply.github.com>
2023-12-09 16:50:27 +0300

    qwe

---
+++ add/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <p v-if="age <= 18">подросток</p>
+  <p v-if="age > 18 & age < 26">молодой человек</p>
+  <p v-if="age >= 26">мужчина</p>
+</template>
+
+<script>
+
+
+export default {
+  data(){
+    return{
+      age: +prompt("введите ваш возраст")
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
